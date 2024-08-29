90e9c9ca283ac0f08456b94d664621fe47d165dd
ranjeet singh <ranjeet.singh@ixambee.com>
2024-08-29 20:30:19 +0530

    add objectIdentifier page

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Chat from './src/pages/Chat';
+import ObjectIdentifier from './src/pages/ObjectIdentifier';
 
 export default function App() {
-  return <Chat />;
+  return <ObjectIdentifier />;
 }
